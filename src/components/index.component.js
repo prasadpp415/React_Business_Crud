1abf26187f77a3dbc574c27c601bdf1f12243ea4
Prasad NM <prasadpp415@gmail.com>
2019-12-05 16:15:53 +0530

    React Business crud added to repo

---
+++ src/components/index.component.js
@@ -0,0 +1,39 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import TableRow from './TableRow';
+
+export default class Index extends Component {
+	constructor(props) {
+		super(props);
+		this.state = { business: [] };
+	}
+
+	componentDidMount() {
+		axios.get('http://localhost:4000/business').then((response) => {
+			this.setState({ business: response.data });
+		});
+	}
+	tableRow() {
+		return this.state.business.map(function(object, i) {
+			return <TableRow obj={object} key={i} />;
+		});
+	}
+	render() {
+		return (
+			<div>
+				<h3 align="center">Business List</h3>
+				<table className="table table-striped" style={{ marginTop: 20 }}>
+					<thead>
+						<tr>
+							<th>Person Name</th>
+							<th>Business Name</th>
+							<th>Business gst number</th>
+							<th colspan="2">Action</th>
+						</tr>
+					</thead>
+					<tbody>{this.tableRow()}</tbody>
+				</table>
+			</div>
+		);
+	}
+}
